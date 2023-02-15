68de42995ef14490076c6e8f3188d25dceac47ea
Joeran Bosma <joeran@bosma.co>
2023-02-15 10:12:41 +0100

    Update naming

--- AbdomenMRUS-prostate-segmentation/export.sh
+++ AbdomenMRUS-prostate-segmentation/export.sh
@@ -2,5 +2,5 @@
 
 ./build.sh
 
-docker save joeranbosma/picai_prostate_segmentation_processor:latest | gzip -c > picai_prostate_segmentation_processor-v2.1.tar.gz
+docker save heviai/picai_prostate_segmentation_processor:latest | gzip -c > picai_prostate_segmentation_processor-v1.0.tar.gz
 
